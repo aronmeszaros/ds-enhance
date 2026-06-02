059b8854092c49d4ba51e671fd7cd81c7df465fb
Aaron <aron.meszaros1@gmail.com>
2026-06-02 14:36:34 +0200

    Initialize DSE_Admin instance in DSE_Plugin constructor

--- includes/class-dse-plugin.php
+++ includes/class-dse-plugin.php
@@ -5,16 +5,19 @@ if (!defined('ABSPATH')) {
 }
 
 require_once DSE_PLUGIN_PATH . 'includes/class-dse-assets.php';
+require_once DSE_PLUGIN_PATH . 'includes/class-dse-admin.php';
 
 class DSE_Plugin
 {
     private static ?DSE_Plugin $instance = null;
 
     private DSE_Assets $assets;
+    private DSE_Admin $admin;
 
     private function __construct()
     {
         $this->assets = new DSE_Assets();
+        $this->admin = new DSE_Admin();
     }
 
     public static function instance(): DSE_Plugin
